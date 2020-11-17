35fb0397a72b9f368905c49df4bb9511e70e9f4d
Joseph <osekuemen@gmail.com>
2020-11-17 18:40:11 +0100

    We create a new EarthquakeRecyclerViewAdapter class that extends RecyclerVIew.Adapter, and within it create a new ViewHolder class that extends RecyclerView.ViewHolder. The ViewHolder will be used to hold a reference to each View from the Earthquake item layout, when you bind the earthquake values to it in the onBindViewHolder method of the Earthquake RecyclerView Adapter.

--- app/src/main/java/com/example/earthquake/EarthquakeListFragment.java
+++ app/src/main/java/com/example/earthquake/EarthquakeListFragment.java
@@ -1,3 +1,5 @@
+package com.example.earthquake;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/example/earthquake/MainActivity.java
+++ app/src/main/java/com/example/earthquake/MainActivity.java
@@ -2,17 +2,42 @@ package com.example.earthquake;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG_LIST_FRAGMENT = "TAG_LIST_FRAGMENT";
 
+    EarthquakeListFragment mEarthquakeListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     { super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earthquake_main);
 
+        FragmentManager fm = getSupportFragmentManager();
 
+        /*In order to maintain a consistent UI state between configuration changes, all the Fragments added
+         to your UI will automatically be restored when an Activity is re-created following an orientation
+        change or unexpected termination.
+        This is particularly important if you are populating your Activity layout with Fragments within
+        the onCreate handler—in which case you must check if the Fragments have already been added to
+        avoid creating multiple copies.
+        You can do this either by checking for Fragments before adding them, or if this is an Activity restart
+        by checking if the savedInstanceState is null:*/
+        if (savedInstanceState == null)
+        {
+            FragmentTransaction ft = fm.beginTransaction();
+
+            mEarthquakeListFragment = new EarthquakeListFragment();
+            ft.add(R.id.main_activity_frame, mEarthquakeListFragment,TAG_LIST_FRAGMENT);
+
+            ft.commitNow();
+        } else
+        {
+            mEarthquakeListFragment =(EarthquakeListFragment) fm.findFragmentByTag(TAG_LIST_FRAGMENT);
+        }
     }
 }
