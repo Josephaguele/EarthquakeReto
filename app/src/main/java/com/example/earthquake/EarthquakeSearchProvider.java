d1a7b34843b4eddb43caf1fa1c471cf8f138caf8
Joseph <osekuemen@gmail.com>
2020-12-11 07:02:06 +0100

    Searching the Earthquake Monitor Database

---
+++ app/src/main/java/com/example/earthquake/EarthquakeSearchProvider.java
@@ -0,0 +1,54 @@
+package com.example.earthquake;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+// To begin searching the Earthquake project, we create the EarthquakeSearchProvider class that
+// extends ContentProvider. It will be used exclusively to generate search suggestions for your
+// Search View. The ContentProvider class has some abstract methods that must be implemented which
+// include getType, query,insert, delete, and update methods.
+
+public class EarthquakeSearchProvider extends ContentProvider
+{
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection,
+                        @Nullable String selection, @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection,
+                      @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values,
+                      @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
