6487f68d0015b160ec2fe0f68bbe9b88c2b4f5be
Joseph <osekuemen@gmail.com>
2020-12-13 00:44:18 +0100

    Searching the Earthquake Monitor Database- STEP 15

--- app/src/main/java/com/example/earthquake/EarthquakeSearchResultActivity.java
+++ app/src/main/java/com/example/earthquake/EarthquakeSearchResultActivity.java
@@ -1,12 +1,15 @@
 package com.example.earthquake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeSearchResultActivity extends AppCompatActivity
 {
@@ -26,5 +29,24 @@ public class EarthquakeSearchResultActivity extends AppCompatActivity
 
     }
 
+    /*Return to the Search Results Activity, and add a new Live Data Observer that will update
+    the Array List of Earthquakes displayed by the Recycler View. Also create a new Mutable
+    Live Data that will store the current search query, and a setSearchQuery method that will
+    modify that query*/
+    MutableLiveData<String> searchQuery;
+
+    private void setSearchQuery(String query)
+    {
+        searchQuery.setValue(query);
+    }
+
+    private final Observer<List<Earthquake>> searchQueryResultObserver = updatedEarthquakes ->
+    {
+        // Update the UI with the updated search query results.
+        mEarthquakes. clear();
+        if (updatedEarthquakes != null)
+            mEarthquakes.addAll(updatedEarthquakes);
+        mEarthquakeAdapter.notifyDataSetChanged();
+    };
 
 }
