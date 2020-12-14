8add9ba720b37b1be10e57de8e5eb02561297d1a
Joseph <osekuemen@gmail.com>
2020-12-14 21:54:31 +0100

    To simply the process of applying updated search terms, we can use Transformations  .switchMap. This method automatically modifies the underlying data of one Live Data based  on changes in another. Apply a Switch Map that monitors the searchQuery Live Data, and  when it changes—update a searchResults Live Data variable, by querying the database   using the updated search term. Then use the Observer from Step 15 to observe changes in the   searchResults Live Data. Finally, extract the search query from the Intent that launched   the Activity, and pass it into the setSearchQuery method.

--- app/src/main/java/com/example/earthquake/EarthquakeSearchResultActivity.java
+++ app/src/main/java/com/example/earthquake/EarthquakeSearchResultActivity.java
@@ -1,11 +1,14 @@
 package com.example.earthquake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.Transformations;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.SearchManager;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -13,6 +16,8 @@ import java.util.List;
 
 public class EarthquakeSearchResultActivity extends AppCompatActivity
 {
+    LiveData<List<Earthquake>> searchResults;
+
     private ArrayList<Earthquake> mEarthquakes = new ArrayList<>();
 
     private EarthquakeRecyclerViewAdapter mEarthquakeAdapter = new EarthquakeRecyclerViewAdapter(mEarthquakes);
@@ -26,6 +31,24 @@ public class EarthquakeSearchResultActivity extends AppCompatActivity
         RecyclerView recyclerView = findViewById(R.id.search_result_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(mEarthquakeAdapter);
+
+        // Initialize the search query Live Data.
+        searchQuery = new MutableLiveData<>();
+        searchQuery.setValue(null);
+
+        // Link the search query Live Data to the search results Live Data.
+        // Configure Switch Map such that a change in the search query
+        // updates the search results by querying the database
+        searchResults = Transformations.switchMap(searchQuery,
+                query -> EarthquakeDatabaseAccessor
+        .getInstance(getApplicationContext()).earthquakeDAO().searchEarthquakes("%" + query + "%"));
+
+        // Observe the changes to the search results Live Data.
+        searchResults.observe(EarthquakeSearchResultActivity.this, searchQueryResultObserver);
+
+        // Extract the search query term and update the search query Live Data
+        String query = getIntent().getStringExtra(SearchManager.QUERY);
+        setSearchQuery(query);
 
     }
 
